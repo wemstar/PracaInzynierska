1216292b83672c4da04fc3fcf0fa7d8c710a2853
wemstar <sylwestermacura@gmail.com>
2014-09-04 21:09:36 +0200

    Dodanie frameworków cz3 (działający BackEnd)

---
+++ BackEnd/src/main/java/edu/agh/fis/SimpleTest.java
@@ -0,0 +1,18 @@
+package edu.agh.fis;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wemstar on 04.09.14.
+ */
+@RestController
+@RequestMapping("/test")
+public class SimpleTest {
+
+    @RequestMapping("/cre")
+    public TestClass creste()
+    {
+        return new TestClass();
+    }
+}
---
+++ BackEnd/src/main/java/edu/agh/fis/TestClass.java
@@ -0,0 +1,10 @@
+package edu.agh.fis;
+
+/**
+ * Created by wemstar on 04.09.14.
+ */
+public class TestClass {
+
+
+    public String txt="text";
+}
